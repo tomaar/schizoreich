26f5517da15b0ade964052e50091ebac81d0f517
tomaar <tomaar19@gmail.com>
2023-12-26 03:37:21 +0100

    fix flag generator

--- scripts/flag_generator.py
+++ scripts/flag_generator.py
@@ -1,11 +1,73 @@
-def load_tags():
-    with open("../../common/country_tags/00_countries.txt") as file:
-        list1 = [line[:3] for line in file]
-    with open("../../common/country_tags/01_countries.txt") as file:
-        list2 = [line[:3] for line in file]
-    with open("../../common/country_tags/new_countries.txt") as file:
-        list3 = [line[:3] for line in file]
-    list = list1 + list2 + list3
-    return list
+import os
+import shutil
 
-print(load_tags())
+def extract_first_three_letters(file_path):
+    result_list = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            # Extract the first 3 letters from each line
+            first_three_letters = line[:3]
+            if first_three_letters[0] == '#' or first_three_letters[0] == '\n' or first_three_letters[0] == ' ':
+                continue
+            result_list.append(first_three_letters)
+    return result_list
+
+def process_directory():
+    current_directory = os.getcwd()
+    directory = os.path.join(current_directory, 'common', 'country_tags')
+    
+    combined_list = []
+    
+    for filename in os.listdir(directory):
+        if filename == 'zz_dynamic_countries.txt' or filename == 'zz_new_dynamic_countries.txt':
+            continue
+        file_path = os.path.join(directory, filename)
+        if os.path.isfile(file_path):
+            # Extract first three letters from each line in the file
+            file_list = extract_first_three_letters(file_path)
+            # Combine the lists
+            combined_list.extend(file_list)
+
+    return combined_list
+
+result_list = process_directory()
+result_list.pop() # remove '\n' from the end of the list
+
+
+working_directory = os.getcwd()
+
+flags_directory = os.path.join(working_directory, 'scripts', 'flags')
+
+if os.path.exists(flags_directory):
+    shutil.rmtree(flags_directory)
+
+os.makedirs(flags_directory)
+os.makedirs(os.path.join(flags_directory, 'medium'))
+os.makedirs(os.path.join(flags_directory, 'small'))
+
+directory = os.path.join(working_directory, 'scripts')
+a = os.path.join(directory, 'ex_big.tga')
+b = os.path.join(directory, 'flags')
+for tag in result_list:
+    shutil.copy(a, b)
+    current_path = os.path.join(b, 'ex_big.tga')
+    new_path = os.path.join(b, tag + '.tga')
+    os.rename(current_path, new_path)
+
+directory = os.path.join(working_directory, 'scripts')
+a = os.path.join(directory, 'ex_medium.tga')
+b = os.path.join(directory, 'flags', 'medium')
+for tag in result_list:
+    shutil.copy(a, b)
+    current_path = os.path.join(b, 'ex_medium.tga')
+    new_path = os.path.join(b, tag + '.tga')
+    os.rename(current_path, new_path)
+
+directory = os.path.join(working_directory, 'scripts')
+a = os.path.join(directory, 'ex_small.tga')
+b = os.path.join(directory, 'flags', 'small')
+for tag in result_list:
+    shutil.copy(a, b)
+    current_path = os.path.join(b, 'ex_small.tga')
+    new_path = os.path.join(b, tag + '.tga')
+    os.rename(current_path, new_path)
